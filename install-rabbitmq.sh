86da49508697a3ce67d473862e0aa0c0ceffb0e5
Pete Cheslock <petecheslock@gmail.com>
2013-08-15 09:38:34 -0400

    Move cert creation to start of container

--- install-rabbitmq.sh
+++ install-rabbitmq.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-git clone https://github.com/joemiller/joemiller.me-intro-to-sensu.git /tmp/sensu-certs
-cd /tmp/sensu-certs/
-./ssl_certs.sh clean
-./ssl_certs.sh generate
-
-mkdir /etc/rabbitmq/ssl
-cp server_key.pem /etc/rabbitmq/ssl/
-cp server_cert.pem /etc/rabbitmq/ssl/
-cp testca/cacert.pem /etc/rabbitmq/ssl/
-
 apt-get -y install erlang-nox
 echo "deb http://www.rabbitmq.com/debian/ testing main" > /etc/apt/sources.list.d/rabbitmq.list
 
